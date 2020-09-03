651bf9fd63eb3c348826ce25489aaab2c18d8d01
elkip <mitchiscool51@gmail.com>
2020-09-03 17:44:42 -0400

    Dependency Injection with Spring

--- src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -1,10 +1,16 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
 
-    public GreetingServiceImpl greetingService;
+    @Qualifier("propertyGreetingService")
+    @Autowired
+    public GreetingService greetingService;
 
     public String getGreeting() {
         return greetingService.sayGreeting();
--- src/test/java/guru/springframework/sfgdi/controllers/SetterInjectionControllerTest.java
+++ src/test/java/guru/springframework/sfgdi/controllers/SetterInjectionControllerTest.java
@@ -1,19 +1,17 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class SetterInjectionControllerTest {
 
-    SetterInjectionController controller;
+    SetterInjectedController controller;
 
     @BeforeEach
     void setUp() {
-        controller = new SetterInjectionController();
-        controller.setGreetingService(new GreetingServiceImpl());
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new ConstructorGreetingService());
     }
 
     @Test
